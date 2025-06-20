f4c3d1797f87329a4bea7c257c14decb7aca65cb
adharsh-1843 <126388050+adharsh-1843@users.noreply.github.com>
2025-06-20 18:20:06 +0530

    add via uplpoad

---
+++ another.py
@@ -0,0 +1,6 @@
+def another_func(num1, num2, result):
+    return {
+        'statusCode': 200,
+        'body': f'The sum of {num1} and {num2} is {result}.'
+    }
+
---
+++ lambda_function.py
@@ -0,0 +1,18 @@
+import json
+from another import another_func
+
+
+
+def lambda_handler(event, context):
+    # Extract numbers from the event
+    num1 = event.get('num1', 0)
+    num2 = event.get('num2', 0)
+
+    # Calculate the sum
+    result = num1 + num2
+    return another_func(num1,num2,result)
+
+    return {
+        'statusCode': 200,
+        'body': f'The sum of {num1} and {num2} is {result}.'
+    }
