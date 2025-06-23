a490c3accf873eb0b47bd294a0d90d422b71a3fb
adharsh-1843 <126388050+adharsh-1843@users.noreply.github.com>
2025-06-23 18:57:38 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -7,7 +7,7 @@ def lambda_handler(event, context):
     # Extract numbers from the event
     num1 = event.get('num1', 0)
     num2 = event.get('num2', 0)
-    print("second ")
+    print("third ")
 
     # Calculate the sum
     result = num1 + num2
